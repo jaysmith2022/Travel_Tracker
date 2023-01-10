5c459a953d4236d69ec6bcd6c09050d3cd870103
jordan smith <jordan.smith6035@gmail.com>
2023-01-10 16:36:42 -0700

    Add describe block and imports for traveler test

--- test/traveler-test.js
+++ test/traveler-test.js
@@ -1,3 +1,15 @@
 import { expect } from "chai";
 import travelerInfo from "../src/data/travelerData";
 import Traveler from "../src/Traveler";
+
+describe("Traveler", function () {
+  let travelerUser;
+
+  beforeEach(() => {
+    travelerUser = new Traveler(travelerInfo[0]);
+  });
+
+  it("Should be a function", function () {
+    expect(Traveler).to.be.a("function");
+  });
+})
