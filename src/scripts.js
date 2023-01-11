25b184ad14dc22b34dce7fa982a7edb40b648750
jordan smith <jordan.smith6035@gmail.com>
2023-01-11 13:54:26 -0700

    Add query selectors for trip details

--- src/scripts.js
+++ src/scripts.js
@@ -1,12 +1,24 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
-
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 import './images/travel-logo.png'
 
+const pastTrips = document.querySelector('#pastTrips')
+const currentTrips = document.querySelector('#currentTrips')
+const pendingTrips = document.querySelector('#pendingTrips')
 
-console.log('This is the JavaScript entry file - your code begins here.');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
