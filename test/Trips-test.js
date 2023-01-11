4945237e3c17277142bb8bb370fe21ae15d3a50d
jordan smith <jordan.smith6035@gmail.com>
2023-01-11 06:00:51 -0700

    Add describe block and before each for testing

--- test/Trips-test.js
+++ test/Trips-test.js
@@ -1,3 +1,17 @@
 import { expect } from "chai";
+import travelerData  from "../src/data/travelerData";
 import tripsData  from "../src/data/tripsData";
+import Traveler from "../src/traveler";
 import Trips from '../src/Trips.js'
+
+
+describe('Repository', function() {
+    let traveler;
+    let tripsInfo;
+
+    beforeEach(() => {
+        traveler = new Traveler(travelerData[0])
+        tripsInfo = new Trips(tripsData)
+    })
+
+})
